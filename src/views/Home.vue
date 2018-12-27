c1a761aca801ed775313e0fded05f3fd452fb5d3
Himanshi Gulati <himanshi.gulati@nagarro.com>
2018-12-27 14:48:56 +0530

    Removed unwanted files and added articles data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home-page">
+
     <div class="banner">
       <div class="container">
         <h1 class="logo-font">conduit</h1>
@@ -9,20 +10,22 @@
 
     <div class="container page">
       <div class="row">
+
         <div class="col-md-9">
           <div class="feed-toggle">
             <ul class="nav nav-pills outline-active">
               <li class="nav-item">
-                <a class="nav-link disabled" href>Your Feed</a>
+                <a class="nav-link disabled" href="">Your Feed</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link active" href>Global Feed</a>
+                <a class="nav-link active" href="">Global Feed</a>
               </li>
             </ul>
           </div>
 
           <ArticlePreview v-for="article in articles" :key="article.slug" :article="article">
           </ArticlePreview>
+
         </div>
 
         <div class="col-md-3">
@@ -30,19 +33,21 @@
             <p>Popular Tags</p>
 
             <div class="tag-list">
-              <a href class="tag-pill tag-default">programming</a>
-              <a href class="tag-pill tag-default">javascript</a>
-              <a href class="tag-pill tag-default">emberjs</a>
-              <a href class="tag-pill tag-default">angularjs</a>
-              <a href class="tag-pill tag-default">react</a>
-              <a href class="tag-pill tag-default">mean</a>
-              <a href class="tag-pill tag-default">node</a>
-              <a href class="tag-pill tag-default">rails</a>
+              <a href="" class="tag-pill tag-default">programming</a>
+              <a href="" class="tag-pill tag-default">javascript</a>
+              <a href="" class="tag-pill tag-default">emberjs</a>
+              <a href="" class="tag-pill tag-default">angularjs</a>
+              <a href="" class="tag-pill tag-default">react</a>
+              <a href="" class="tag-pill tag-default">mean</a>
+              <a href="" class="tag-pill tag-default">node</a>
+              <a href="" class="tag-pill tag-default">rails</a>
             </div>
           </div>
         </div>
+
       </div>
     </div>
+
   </div>
 </template>
 
@@ -94,4 +99,3 @@ export default {
   }
 }
 </script>
-
