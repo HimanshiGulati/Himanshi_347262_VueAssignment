edf96f0df768dd78228f13e9781d0e48e3f86c12
Himanshi Gulati <himanshi.gulati@nagarro.com>
2018-12-27 15:22:24 +0530

    Added Nav Footer

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
   <div id="app">
     <NavBar></NavBar>
     <router-view/>
+    <NavFooter></NavFooter>
   </div>
 </template>
 <script>
 import NavBar from "@/components/NavBar.vue";
+import NavFooter from "@/components/NavFooter.vue";
 
 export default {
-  components:{  
-    NavBar
+  components: {
+    NavBar, NavFooter
   }
 };
 </script>
 
 <style>
+
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,10 +15,10 @@
           <div class="feed-toggle">
             <ul class="nav nav-pills outline-active">
               <li class="nav-item">
-                <a class="nav-link disabled" href="">Your Feed</a>
+                <a class="nav-link" v-if="username" @click="setFeed( 'user');" :class="{active:activeFeed==='user' } ">Your Feed</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link active" href="">Global Feed</a>
+              <li class="nav-item ">
+                <a class="nav-link" @click="setFeed( 'global');" :class="{active:activeFeed==='global' } ">Global Feed</a>
               </li>
             </ul>
           </div>
@@ -56,6 +56,28 @@ import ArticlePreview from "@/components/ArticlePreview.vue";
 export default {
   components: {
     ArticlePreview
+  },
+  methods: {
+    setFeed(feedType) {
+      if (feedType === "global") {
+        this.activeFeed = "global";
+        this.$store.dispatch("articles/getGlobalFeed");
+      } else if (feedType === "user") {
+        this.activeFeed = "user";
+        this.$store.dispatch("articles/getUserFeed")
+      }
+    }
+  },
+  created() {
+    this.setFeed("global");
+  },
+  computed: {
+    globalArticles() {
+      return this.$store.state.articles.feed || [];
+    },
+    username() {
+      return this.$store.getters["users/username"];
+    }
   },
   data: function() {
     return {
